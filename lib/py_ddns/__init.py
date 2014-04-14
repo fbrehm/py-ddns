743bf8533c814079e36ad5decec3741cd3306237
Frank Brehm <frank@brehm-online.com>
2014-04-14 21:47:26 +0200

    Added lib/py_ddns/__init.py

---
+++ lib/py_ddns/__init.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+@author: Frank Brehm
+@contact: frank.brehm@profitbricks.com
+@copyright: © 2010 - 2014 by Frank Brehm, Berlin
+@summary: All modules for Python DynDNS CGI application
+"""
+
+__author__ = 'Frank Brehm <frank.brehm@profitbricks.com>'
+__copyright__ = '(C) 2010 - 2014 by Frank Brehm, Berlin'
+__contact__ = 'frank.brehm@profitbricks.com'
+__version__ = '0.1.0'
+__license__ = 'LGPLv3+'
+
+#==============================================================================
+
+if __name__ == "__main__":
+
+    pass
+
+#==============================================================================
+
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
